70a0ce24cc6a38b4d27d929bf4b3144d0cee227d
tfjackc <colpitt.jack@gmail.com>
2023-04-28 01:30:00 -0700

    map is increasing in functionality

--- pats/urls.py
+++ pats/urls.py
@@ -6,7 +6,7 @@ appname = 'pats'
 urlpatterns = [
     path('', views.index, name='index'),
     path('map/', views.mapPage, name='mapPage'),
-    #path('get/ajax/mapData', views.mapRequest, name='mapRequest'),
+    #path('map/get/mapData', views.account_data, name='account_data'),
     path('<str:account>/', views.account_query, name='account_query'),
     path('<str:account>/valuation/', views.valuation, name='valuation'),
    
