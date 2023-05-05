ed4ffa8427c0038d25305386ea436b3ca22850fb
tfjackc <colpitt.jack@gmail.com>
2023-05-04 23:39:30 -0700

    leafmap integrated

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
-#from portfolio.views import FoliumView
+from portfolio.views import FoliumView
 
 appname = 'portfolio'
 
 urlpatterns = [
     path('', views.base, name='base'),
-    #path('map', FoliumView.as_view()),
+    path('map', FoliumView.as_view()),
     path('jackcolpitt/', views.contact_create_view, name='contact_create_view'),
     path('jackcolpitt/notebook/', views.notebook, name='notebook'),
     path('jackcolpitt/resume/', views.resume, name='resume'),
