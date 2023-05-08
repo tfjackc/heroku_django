0243a4aff7df965dc76e8e9c1a0b51a087cae412
tfjackc <86210047+tfjackc@users.noreply.github.com>
2023-05-08 10:33:28 -0700

    Update settings.py

--- crook/settings.py
+++ crook/settings.py
@@ -35,7 +35,7 @@ IS_HEROKU = "DYNO" in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.getenv("DEBUG", "False") == "False"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 if IS_HEROKU:
     ALLOWED_HOSTS = ["*"]
