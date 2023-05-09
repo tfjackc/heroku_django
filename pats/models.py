046d5113ddd70bbed7b85767f6b4968a8baa93c7
Jack Colpitt <JColpitt@cc1.com>
2023-05-08 22:32:05 -0700

    updating models

--- pats/models.py
+++ pats/models.py
@@ -103,6 +103,7 @@ class ContactMich(models.Model):
     name = models.CharField(max_length=50, blank=True, null=True)
     email = models.CharField(max_length=100, blank=True, null=True)
     message = models.CharField(max_length=1000, blank=True, null=True)
+    submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return (
