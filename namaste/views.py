76fa2165f72b6de029c67e081c30dcf48bbd6e5b
tfjackc <colpitt.jack@gmail.com>
2023-05-09 16:58:39 -0700

    working with models

--- namaste/views.py
+++ namaste/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 import json
 import requests
 from pats.models import ContactMich
