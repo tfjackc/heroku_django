de0a60e09120fe5f6140338ae0b53722a9de74a8
tfjackc <colpitt.jack@gmail.com>
2023-06-10 23:52:41 -0700

    geojson machine tweaked

--- portfolio/views.py
+++ portfolio/views.py
@@ -165,7 +165,7 @@ class Draw(JSCSSMixin, MacroElement):
         }
         </style>
         """
-        export_button = """<a href='#' class="btn btn-info" id='export'>Export</a>"""
+        export_button = """<a href='#' class="btn btn-info" id='export' style="font-size: large;">Export</a>"""
         if self.export:
             figure.header.add_child(Element(export_style), name="export")
             figure.html.add_child(Element(export_button), name="export_button")
